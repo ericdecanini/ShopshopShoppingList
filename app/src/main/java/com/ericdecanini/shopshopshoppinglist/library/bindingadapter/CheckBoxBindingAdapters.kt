84ba10c9108b182efaa849d450cd5bee55238afb
ericdecanini <eddecanini@gmail.com>
2021-03-12 07:42:50 +0000

    Fixes checkbox double checking on click

--- app/src/main/java/com/ericdecanini/shopshopshoppinglist/library/bindingadapter/CheckBoxBindingAdapters.kt
+++ app/src/main/java/com/ericdecanini/shopshopshoppinglist/library/bindingadapter/CheckBoxBindingAdapters.kt
@@ -4,9 +4,14 @@ import android.widget.CheckBox
 import androidx.databinding.BindingAdapter
 import com.ericdecanini.shopshopshoppinglist.util.UiEventListeners
 
-@BindingAdapter("app:onChecked")
-fun CheckBox.onChecked(callback: UiEventListeners.OnCheckChangedListener) {
+@BindingAdapter(value=["app:onChecked", "app:clearsFocusOnCheck"], requireAll = false)
+fun CheckBox.onChecked(
+    callback: UiEventListeners.OnCheckChangedListener,
+    clearsFocus: Boolean = false
+) {
     setOnCheckedChangeListener { _, _ ->
+        if (clearsFocus)
+            rootView.findFocus()?.clearFocus()
         callback.onCheckChanged(this)
     }
 }
