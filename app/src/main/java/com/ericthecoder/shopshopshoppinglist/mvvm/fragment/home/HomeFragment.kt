797911b7ed31fd5cef351345064a2d8f9bc767db
ericdecanini <eddecanini@gmail.com>
2021-12-30 15:24:21 +0100

    adds toolbar casting on HomeFragment

--- app/src/main/java/com/ericthecoder/shopshopshoppinglist/mvvm/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/ericthecoder/shopshopshoppinglist/mvvm/fragment/home/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.ericthecoder.shopshopshoppinglist.mvvm.fragment.home
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -42,7 +43,7 @@ class HomeFragment : DaggerFragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
         binding.setVariable(BR.viewmodel, viewModel)
         binding.lifecycleOwner = this
-        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar)
+        (requireActivity() as AppCompatActivity).setSupportActionBar(binding.toolbar as Toolbar)
 
         initShoppingLists()
         observeState()
