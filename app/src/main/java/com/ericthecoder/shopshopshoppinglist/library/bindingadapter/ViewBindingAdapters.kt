a454054a27df795d1395617527f82a0aeb7de072
ericdecanini <eddecanini@gmail.com>
2022-04-16 18:11:13 +0200

    Hides keyboard on clear focus

--- app/src/main/java/com/ericthecoder/shopshopshoppinglist/library/bindingadapter/ViewBindingAdapters.kt
+++ app/src/main/java/com/ericthecoder/shopshopshoppinglist/library/bindingadapter/ViewBindingAdapters.kt
@@ -1,14 +1,23 @@
 package com.ericthecoder.shopshopshoppinglist.library.bindingadapter
 
+import android.content.Context
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.BindingAdapter
+
 
 @BindingAdapter("app:clearsFocusOnClick")
 fun View.clearsFocusOnClick(clearsFocus: Boolean) {
     if (clearsFocus)
         setOnTouchListener { _, _ ->
             rootView.findFocus()?.clearFocus()
+            hideKeyboard(this)
             performClick()
             true
         }
+}
+
+private fun hideKeyboard(view: View) {
+    val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
 }
