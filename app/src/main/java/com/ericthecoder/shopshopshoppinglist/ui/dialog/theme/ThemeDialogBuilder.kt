e070433d0b6bc796f93640a3f34b4b66bc4481f5
ericdecanini <eddecanini@gmail.com>
2022-05-06 17:58:52 +0200

    Toggles dynamic button visibility

--- app/src/main/java/com/ericthecoder/shopshopshoppinglist/ui/dialog/theme/ThemeDialogBuilder.kt
+++ app/src/main/java/com/ericthecoder/shopshopshoppinglist/ui/dialog/theme/ThemeDialogBuilder.kt
@@ -1,10 +1,13 @@
 package com.ericthecoder.shopshopshoppinglist.ui.dialog.theme
 
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.ericthecoder.shopshopshoppinglist.BR
 import com.ericthecoder.shopshopshoppinglist.R
 import com.ericthecoder.shopshopshoppinglist.databinding.DialogThemeBinding
 import com.ericthecoder.shopshopshoppinglist.entities.theme.Theme
+import com.google.android.material.color.DynamicColors
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 object ThemeDialogBuilder {
@@ -13,9 +16,7 @@ object ThemeDialogBuilder {
     var selectedTheme = Theme.BLUE
 
     fun show(activity: AppCompatActivity, onThemeSelected: (Theme) -> Unit) {
-        val binding = DialogThemeBinding.inflate(activity.layoutInflater)
-        binding.setVariable(BR.listener, createThemeSelectedListener(binding))
-        binding.setVariable(BR.selectedTheme, selectedTheme)
+        val binding = inflateBinding(activity.layoutInflater)
 
         MaterialAlertDialogBuilder(activity)
             .setTitle(activity.getString(R.string.theme_dialog_title))
@@ -23,6 +24,17 @@ object ThemeDialogBuilder {
             .setPositiveButton(R.string.restart) { _, _ -> selectTheme(onThemeSelected) }
             .setNegativeButton(R.string.cancel) { _, _ -> selectedTheme = currentTheme }
             .show()
+    }
+
+    private fun inflateBinding(layoutInflater: LayoutInflater): DialogThemeBinding {
+        val binding = DialogThemeBinding.inflate(layoutInflater)
+        binding.setVariable(BR.listener, createThemeSelectedListener(binding))
+        binding.setVariable(BR.selectedTheme, selectedTheme)
+        if (!DynamicColors.isDynamicColorAvailable()) {
+            binding.themeDynamic.isVisible = false
+        }
+
+        return binding
     }
 
     private fun selectTheme(onThemeSelected: (Theme) -> Unit) {
