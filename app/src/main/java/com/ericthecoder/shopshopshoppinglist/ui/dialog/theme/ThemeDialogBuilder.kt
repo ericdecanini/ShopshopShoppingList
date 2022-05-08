b44c46f2c92b4b44bd4e2594fc351ab376f8c96a
ericdecanini <eddecanini@gmail.com>
2022-05-08 16:47:26 +0200

    Changes default colour to blue

--- app/src/main/java/com/ericthecoder/shopshopshoppinglist/ui/dialog/theme/ThemeDialogBuilder.kt
+++ app/src/main/java/com/ericthecoder/shopshopshoppinglist/ui/dialog/theme/ThemeDialogBuilder.kt
@@ -12,7 +12,7 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 object ThemeDialogBuilder {
 
-    var selectedTheme = Theme.GREEN
+    var selectedTheme = Theme.BLUE
 
     fun show(activity: AppCompatActivity, currentTheme: Theme, onThemeSelected: (Theme) -> Unit) {
         selectedTheme = currentTheme
